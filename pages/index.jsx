359fa97ad01e719c6a371183eeb1a87eafd1d0a7
miillaadddddd <milad7212@gmail.com>
2022-10-31 13:07:39 +0330

    add first page

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-indigo-600 justify-center items-center">
+    <div className=" min-h-screen bg-ciBackground p-4 ">
+      <div className="flex flex-col items-center justify-center mt-6 text-ciForeground">
+        <span className="w-40 h-40 bg-ciBackgroundTwo rounded-full"></span>
+        <p className="font-bold text-3xl my-5 text-ciPink">مهندسی عمران</p>
+        <p className=" font-bold text-xl animate-pulse">راحتتر مهندسی کن</p>
+      </div>
       <p className="text-white font-bold ">شروع 5</p>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        ciBackground:'#282a36',
+        ciBackgroundTwo:'#383A59',
+        ciCurrentLine:'#44475a',
+        ciForeground:'#f8f8f2',
+        ciComment:'#6272a4',
+        ciCyan:'#8be9fd',
+        ciGreen:'#50fa7b',
+        ciOrange:'#ffb86c',
+        ciPink:'#ff79c6',
+        ciPurple:'#bd93f9',
+        ciRed:'#ff5555',
+        ciYellow:'#f1fa8c'
+
+      }
+    },
   },
   plugins: [],
 }
