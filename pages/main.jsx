826d1b5aa8cad0c915ad401a2bc27ba01805e71e
miillaadddddd <milad7212@gmail.com>
2022-10-31 15:10:26 +0330

    change style

--- pages/main.jsx
+++ pages/main.jsx
@@ -7,15 +7,20 @@ function main() {
       <p className=" text-ciCyan font-bold text-xl">دفترچه خاک</p>
 
       {StudySteps.map((item, index) => (
-        <div key={index} className="my-10">
-            <p className="text-ciGreen font-bold">{item.title}  [ {item.children.length} ]</p>
-            <div className="grid grid-cols-4 gap-x-2">
-                {item.children.map((childItem,index)=>(
-                    <div key={index} className="bg-ciForeground  rounded-lg my-2 p-4">
-                        <p className="font-bold">{childItem.title}</p>
-                    </div>
-                ))}
-            </div>
+        <div key={index} className="my-10 ">
+          <p className="text-ciGreen font-bold">
+            {item.title} [ {item.children.length} ]
+          </p>
+          <div className="grid grid-cols-4 gap-x-2">
+            {item.children.map((childItem, index) => (
+              <div
+                key={index}
+                className="bg-ciForeground cursor-pointer hover:bg-ciCyan duration-200 ease-in rounded-lg my-2 p-4"
+              >
+                <p className="font-bold">{childItem.title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
