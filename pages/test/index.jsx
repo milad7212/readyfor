fdeb7fdcf10f7780430c4dc266a1ff44616961ef
miillaadddddd <milad7212@gmail.com>
2022-11-05 10:36:48 +0330

    change styles in testPage

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -11,26 +11,39 @@ function TestPage() {
       setWitchQuestion(newTest)
     }
   }
+  function changeCertainQuestion (value){
+    setWitchQuestion(value)
+  }
+
+  function choiceStyle(value){
+    if (value == witchQuestion){
+      return 'bg-ciForeground shadow-lg  '
+    }else{
+      return 'bg-gray-300'
+    }
+  }
 
   return (
-    <div className=" min-h-screen bg-ciBackground p-4 relative flex justify-center items-center">
-      <p className="absolute right-0 top-0 p-4 text-ciTiter">
+    <div className=" min-h-screen bg-ciBackground p-4 relative flex flex-col items-center ">
+      <p className="absolute right-0  top-0 p-4 text-ciTiter ">
         آزمون عمران اجرا اردیبهشت 97
       </p>
       <div className="absolute left-0 top-0 text-ciTiter flex p-4">
         <p className="ml-4">زمان باقی مانده :</p>
         <p className="text-ciForeground font-bold">2:54:36</p>
       </div>
-      <div className=" absolute top-11 w-full flex  flex-wrap gap-1 p-4" style={{direction:'ltr'}}>
+      <div className="  mt-8 w-full flex  flex-wrap gap-1 p-4" style={{direction:'ltr'}}>
         {new Array(60).fill(1).map((item,index)=>(
-          <span key={index} className="bg-gray-300 w-6 h-6 rounded-lg flex justify-center items-center text-xs font-bold">{index+1}</span>
+          <span onClick={()=>changeCertainQuestion(index)} key={index} className={`${choiceStyle(index)} w-6 h-6 rounded-lg flex justify-center items-center text-xs font-bold cursor-pointer`}>{index+1}</span>
         ))}
       </div>
 
-      <ItemTest
-        data={testsData[witchQuestion]}
-        changeQuestion={changeQuestion}
-      />
+      <div className="mt-6">
+        <ItemTest
+          data={testsData[witchQuestion]}
+          changeQuestion={changeQuestion}
+        />
+      </div>
     </div>
   );
 }
