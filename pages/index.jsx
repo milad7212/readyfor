86c5603bb71e9158a0b2229d2c88ff710c12ccfb
miillaadddddd <milad7212@gmail.com>
2022-11-20 12:57:17 +0330

    responsive first page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,26 +6,27 @@ import { CategoryData } from "../data/data";
 export default function Home() {
   return (
     <div className=" min-h-screen bg-ciBackground p-4 ">
-      <div className="flex flex-col items-center justify-center mt-6 text-ciForeground">
-        
-        <img className="w-40 h-40 bg-ciBackgroundTwo rounded-full" src="/images/foundation/main2.webp" alt="" />
-        
-        <p className="font-bold text-3xl my-5 text-ciPink">مهندسی عمران</p>
-        <p className=" font-bold text-xl animate-pulse">راحتتر مهندسی کن</p>
+      <div className=" flex flex-col items-center justify-center text-ciForeground">
+        <img
+          className="h-20 w-20    md:h-40 md:w-40 rounded-full bg-ciBackgroundTwo"
+          src="/images/foundation/main2.webp"
+          alt=""
+        />
+
+        <p className=" md:my-5 my-2 md:text-3xl font-bold text-ciPink">مهندسی </p>
+        <p className=" animate-pulse md:text-xl font-bold">راحتتر مهندسی کن</p>
       </div>
-      <section className="grid grid-cols-3 gap-8 gap-y-16  my-20 max-w-5xl mx-auto">
+      <section className="my-8 mx-auto grid max-w-5xl grid-cols-2  md:grid-cols-6 gap-8 ">
         {CategoryData.map((item, index) => (
           <>
-          
             <Link href={item.link}>
-              <div className="flex  justify-center items-center flex-col cursor-pointer ">
-
-              <div className="w-36 h-36    overflow-hidden ">
-              <img src={item.img} alt="" />
-              </div>
-              <p className="text-ciTiter mt-1 font-bold text-2xl">
-                {item.name}
-              </p>
+              <div className="flex  cursor-pointer flex-col items-center justify-center ">
+                <div className="h-28 w-28    overflow-hidden ">
+                  <img src={item.img} alt="" />
+                </div>
+                <p className="mt-1 md:text-2xl font-bold text-ciTiter">
+                  {item.name}
+                </p>
               </div>
             </Link>
           </>
